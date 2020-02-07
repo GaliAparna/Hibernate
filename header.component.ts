b445c86d80c40a6fb786ebb3f0d475340dc1d411
GaliAparna <60088550+GaliAparna@users.noreply.github.com>
2020-02-07 00:06:40 -0800

    Add files via upload

--- header.component.ts
+++ header.component.ts
@@ -5,17 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent  {
-
-  webTitle:string;
-  webImage:string;
-  webText:string;
+export class HeaderComponent {
+  title:string;
 
   constructor() { 
-    this.webTitle="Angular";
-    this.webText="welcome to angular";
-    this.webImage="assets/images/image1.jpg";
+    this.title="Cognizant";
   }
 
-  
+ 
+
 }
